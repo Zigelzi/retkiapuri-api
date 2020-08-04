8bbd40981a61824c4f4d5368e29632a622774fe4
zigelzi <miika.a.savela@gmail.com>
2020-08-04 11:38:01 +0300

    Add NationalPark model and routes

--- api/models.py
+++ api/models.py
@@ -1,11 +1,36 @@
 from api import db
 
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+from marshmallow_sqlalchemy.fields import Nested
+
+class NationalPark(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    longitude = db.Column(db.Float)
+    latitude = db.Column(db.Float)
+
+    routes = db.relationship('HikeRoute', backref='national_park', lazy=True)
+
+    def save(self):
+        db.session.add(self)
+    
+    def delete(self):
+        db.session.delete(self)
+
+    @staticmethod
+    def get_all():
+        return NationalPark.query.all()
+
+    def __repr__(self):
+        return f'<NationalPark {self.name} | Longitude {self.longitude} | Latitude {self.latitude}>'
 
 class HikeRoute(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(), nullable=False)
-    length = db.Column(db.Float(precision=2))
+    name = db.Column(db.String(100), nullable=False)
+    length = db.Column(db.Float(precision=2), nullable=False)
+    difficulty = db.Column(db.String(20), nullable=False)
+
+    national_park_id = db.Column(db.Integer, db.ForeignKey('national_park.id'))
 
     def save(self):
         db.session.add(self)
@@ -26,5 +51,14 @@ class HikeRoute(db.Model):
 class HikeRouteSchema(SQLAlchemyAutoSchema):
     class Meta:
         model = HikeRoute
+        include_fk = True
         sqla_session = db.session
         load_instance = True
+
+class NationalParkSchema(SQLAlchemyAutoSchema):
+    hike_routes = Nested(HikeRouteSchema, many=True)
+    class Meta:
+        model = NationalPark
+        include_fk = True
+        load_instance = True
+        sqla_session = db.session
--- api/routes.py
+++ api/routes.py
@@ -2,9 +2,12 @@ from flask import make_response, jsonify, request
 from api import app, db
 import traceback
 
-from api.models import HikeRoute, HikeRouteSchema
+from api.models import NationalPark, NationalParkSchema, HikeRoute, HikeRouteSchema
 
 # Schemas for serialisation
+national_park_schema = NationalParkSchema()
+many_national_parks_schema = NationalParkSchema(many=True)
+
 hike_route_schema = HikeRouteSchema()
 many_hike_routes_schema = HikeRouteSchema(many=True)
 
@@ -13,6 +16,71 @@ status_msg_fail = 'fail'
 status_msg_success = 'success'
 
 # Routes
+
+@app.route('/api/national_park', methods=['POST'])
+def add_national_park():
+    try:
+        response_object = {'status': status_msg_success }
+        request_data = request.get_json()
+        national_park = national_park_schema.load(request_data)
+        if national_park:
+            national_park.save()
+            db.session.commit()
+            response_object['national_park'] = national_park_schema.dump(national_park)
+            response_object['message'] = 'National park added successfully!'
+            json_response = jsonify(response_object)
+            return make_response(json_response, 200)
+        else:
+            response_object['status'] = status_msg_fail
+            response_object['message'] = 'Request contains incorrect data, check response.'
+    except Exception as e:
+        traceback.print_exc()
+        response_object['status'] = status_msg_fail
+        response_object['message'] = 'Something went wrong when trying to add national park'
+        db.session.rollback()
+        json_response = jsonify(response_object)
+        return make_response(json_response, 400)
+
+@app.route('/api/national_park/<int:national_park_id>', methods=['GET'])
+def get_national_park(national_park_id):
+    response_object = {'status': status_msg_success }
+    try:
+        national_park = NationalPark.query.get(national_park_id)
+        if national_park:
+            national_park_object = national_park_schema.dump(national_park)
+            response_object['national_park'] = national_park_object
+            response_object['message'] = 'National park queried successfully!'
+            json_response = jsonify(response_object)
+            return make_response(json_response, 200)
+        elif task == None:
+            response_object['status'] = status_msg_fail
+            response_object['message'] = 'Queried national park was not found'
+            json_response = jsonify(response_object)
+            return make_response(json_response, 404)
+    except Exception as e:
+        traceback.print_exc()
+        response_object['status'] = status_msg_fail
+        response_object['message'] = 'Something went wrong when trying to fetch national park'
+        json_response = jsonify(response_object)
+        return make_response(json_response, 400)
+
+@app.route('/api/national_parks', methods=['GET'])
+def get_all_national_parks():
+    response_object = {'status': status_msg_success }
+    try:
+        national_parks = NationalPark.get_all()
+        print(national_parks)
+        national_parks_object = many_national_parks_schema.dump(national_parks)
+        response_object['national_parks'] = national_parks_object
+        response_object['message'] = 'National parks queried successfully!'
+        json_response = jsonify(response_object)
+        return make_response(json_response, 200)
+    except Exception as e:
+        traceback.print_exc()
+        response_object['status'] = status_msg_fail
+        response_object['message'] = 'Something went wrong while querying for all national parks'
+        json_response = jsonify(response_object)
+        return make_response(json_response, 400)
 
 @app.route('/api/hike_route', methods=['POST'])
 def add_hike_route():
@@ -65,7 +133,7 @@ def get_hike_route(hike_route_id):
 def get_all_hike_routes():
     response_object = {'status': status_msg_success }
     try:
-        hike_routes = HikeRoute.query.all()
+        hike_routes = HikeRoute.get_all()
         hike_routes_object = many_hike_routes_schema.dump(hike_routes)
         response_object['hike_routes'] = hike_routes_object
         response_object['message'] = 'Hike routes queried successfully!'
